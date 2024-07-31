90b3307f5c6c63bf959fa1626e49a4ee18f3f71b
SeanSean <122302115+CBL-nguyenson@users.noreply.github.com>
2024-08-01 00:25:01 +0700

    CBL_2024V1

--- examples/IoTStarterKit_Chuyen_Bao_Loc_2024/IoTStarterKit_Chuyen_Bao_Loc_2024.ino
+++ examples/IoTStarterKit_Chuyen_Bao_Loc_2024/IoTStarterKit_Chuyen_Bao_Loc_2024.ino
@@ -1,9 +1,4 @@
-/*
-  IoTStarterKit_Chuyen_Bao_Loc_2024
-  Title : Xây dựng hệ thống IOT quy mô hộ gia đình trên nền tảng Blynk
-  Author: Nguyễn Sơn
-  Date  : 29/7/2024
-*/
+
 
 /* ------------------------------------------------------------------------- */
 /*                                   DEFINE                                  */
@@ -12,7 +7,8 @@
 //######## Các chân sau đã được kết nối sẵn với driver động cơ ########
 //################## Không thay đổi chân (PIN GPIO) ###################
 
-#define LED 13
+
+
 //----Motor_A----
 #define in1 9  // L298n Motor Driver pins.
 #define in2 8
@@ -22,17 +18,21 @@
 #define in4 4
 #define pwm_2 5
 
+
 //#####################################################################
 //#####################################################################
+
+
 
 
 // Định nghĩa các chân GPIO cho vi điều khiển
 #define RELAY_PIN 11  // Chân điều khiển RELAY
-#define LED_PIN 12    // Chân điều khiển LED
+#define LED_PIN 10    // Chân điều khiển LED
 
-#define BLYNK_TEMPLATE_ID "TMPL60Nqrr_Vx"
-#define BLYNK_TEMPLATE_NAME "Den noi that"
-#define BLYNK_AUTH_TOKEN "J_kNkJtUkpajotfENg5OREqSv7cQAMvX"
+
+#define BLYNK_TEMPLATE_ID "TMPL6udENkXYk"
+#define BLYNK_TEMPLATE_NAME "IT"
+#define BLYNK_AUTH_TOKEN "XTybg-3Nm1BjnHSxvTD-WKpE91ldCm_g"
 
 
 // Bao gồm các thư viện cần thiết
@@ -41,24 +41,30 @@
 
 // Thiết lập WiFi
 char auth[] = BLYNK_AUTH_TOKEN;    // Mã xác thực Blynk
-char ssid[] = "phone";  // Tên mạng Wi-Fi (chỉ hoạt động với băng tần 2.4Ghz)
-char pass[] = "1234567890";         // Mật khẩu Wi-Fi
+char ssid[] = "QuangHop";  // Tên mạng Wi-Fi (chỉ hoạt động với băng tần 2.4Ghz)
+char pass[] = "12345678";         // Mật khẩu Wi-Fi
+
+
 
 
 // Định nghĩa các chân Virtual Pins như trên Web BLYNK
-const int Virtual_Pins_LED = 1;
+const int Virtual_Pins_LED = 0;
 const int Virtual_Pins_Relay = 2;
-const int Virtual_Pins_Motor = 3;
+const int Virtual_Pins_Motor = 1;
+
 
 unsigned long lastTimeSend = 0;  // Biến thời gian gửi dữ liệu cuối cùng
+
 
 int ledState = 0;     // Biến trạng thái của LED (bật/tắt)
 int relayState = 0;   // Biến trạng thái của Relay (bật/tắt)
 int motor_speed = 0;  // Biến lưu giá trị tốc độ motor
 
+
 /* ------------------------------------------------------------------------- */
 /*                                   Set_Up                                  */
 /* ------------------------------------------------------------------------- */
+
 
 void setup() {
   pinMode(in1, OUTPUT);
@@ -68,23 +74,29 @@ void setup() {
   pinMode(pwm_1, OUTPUT);
   pinMode(pwm_2, OUTPUT);
 
+
   pinMode(LED_PIN, OUTPUT);    // Thiết lập chân LED là OUTPUT
   pinMode(RELAY_PIN, OUTPUT);  // Thiết lập chân module Relay là OUTPUT
+
 
   Serial.begin(115200);                   // Khởi tạo giao tiếp Serial để debug
   Serial.println("Start BlynkGate I2C");  // In ra thông báo khởi tạo BlynkGate I2C
 
+
   Blynk.begin(auth, ssid, pass);  // Kết nối với Blynk bằng thông tin xác thực và Wi-Fi
 }
+
 
 /* ------------------------------------------------------------------------- */
 /*                                   MAIN                                    */
 /* ------------------------------------------------------------------------- */
 
+
 void loop() {
   Blynk.run();  // Xử lý các yêu cầu từ Blynk, không sử dụng delay trong loop này
   digitalWrite(LED_PIN, ledState);       // Cập nhật trạng thái của LED (bật/tắt)
   digitalWrite(RELAY_PIN, !relayState);  // Cập nhật trạng thái của relay (bật/tắt)
+
 
   // Điều khiển Motor_A
   if (motor_speed > 0) {
@@ -104,10 +116,12 @@ void loop() {
 /* ------------------------------------------------------------------------- */
 /* ------------------------------------------------------------------------- */
 
+
 BLYNK_WRITE_DEFAULT() {  // Nhận giá trị từ Virtual Pin
   // double myDouble = param.asFloat();  // Nhận giá trị từ Blynk và chuyển đổi thành kiểu float
   // String myString = param.asString();
   int myInt = param.asInt();  // Nhận giá trị từ Blynk và chuyển đổi thành kiểu int
+
 
   Serial.print("input V");    // In ra pin đầu vào
   Serial.print(request.pin);  // In ra số pin
@@ -121,7 +135,6 @@ BLYNK_WRITE_DEFAULT() {  // Nhận giá trị từ Virtual Pin
       ledState = 0;                       // Tắt LED
     }
   }
-
   if (request.pin == Virtual_Pins_Relay) {  // Kiểm tra nếu là Virtual_Pins_Relay
     if (myInt == 1) {                       // Nếu giá trị nhận được là 1
       relayState = 1;                       // Bật Relay
@@ -129,7 +142,6 @@ BLYNK_WRITE_DEFAULT() {  // Nhận giá trị từ Virtual Pin
       relayState = 0;                       // Tắt Relay
     }
   }
-
   if (request.pin == Virtual_Pins_Motor) {  // Kiểm tra nếu là Virtual_Pins_Motor
     motor_speed = myInt;                    // Lưu giá trị vào biến motor_speed
   }
